53ee6c74bf16c3e76b39e6788eb5e33042efd6a5
Darrell Bircsak <dbircsak@users.noreply.github.com>
2025-09-24 12:36:36 -0700

    Init commit

---
+++ freezetag.bat
@@ -0,0 +1,10 @@
+@echo off
+set Q2DIR=C:\Program Files (x86)\Steam\steamapps\common\Quake 2
+set "MODDIR=%Q2DIR%\rerelease\freezetag"
+
+if not exist "%MODDIR%" (
+	mkdir "%MODDIR%"
+)
+
+copy /Y "game_x64.dll" "%MODDIR%\"
+start "" /D "%Q2DIR%" quake2.exe +set game freezetag +map strike +set deathmatch 1 +set teamplay 1 +set developer 1 +addbot +addbot +addbot +addbot +addbot +addbot +addbot
